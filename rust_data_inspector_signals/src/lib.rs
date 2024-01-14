665f2e3df04a867d7142d4c95cdb908b8c44c100
Luca Erbetta <luca.erbetta105@gmail.com>
2024-01-14 17:45:26 +0100

    export SignalError

--- rust_data_inspector_signals/src/lib.rs
+++ rust_data_inspector_signals/src/lib.rs
@@ -1,3 +1,3 @@
 mod signal;
 
-pub use signal::{Signal, SignalID, SignalProducer, SignalSample, SignalSendError, Signals};
+pub use signal::{Signal, SignalError, SignalID, SignalProducer, SignalSample, SignalSendError, Signals};
